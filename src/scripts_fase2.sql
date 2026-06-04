7250426364ebda13a8ea6be4a46607489788d35a
Julio Fernandes <jaguarfalcao93@gmail.com>
2026-06-04 22:39:00 +0100

    fase 2.2: criacao da tabela alunos e insercao de dados

--- src/scripts_fase2.sql
+++ src/scripts_fase2.sql
@@ -21,3 +21,21 @@ VALUES
     ('Java Básico', 'Curso para iniciante, domine as bases de Java', 40, 15000.00),
     ('Spring Boot', 'Curso completo de Spring Boot, adquira habilidades para criar aplicações robustas', 60, 25000.00),
     ('Banco de Dados', 'SQL e modelagem', 30, 10000.00);
+
+
+-- Criando tabela de alunos
+CREATE TABLE alunos (
+                        id SERIAL PRIMARY KEY,
+                        nome VARCHAR(100) NOT NULL,
+                        email VARCHAR(100),
+                        telefone VARCHAR(20),
+                        data_nascimento DATE,
+                        data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Inserindo alunos
+INSERT INTO cursos (nome, email, telefone, data_nascimento)
+VALUES
+    ('Julio Fernandes', 'julio@jaguar.ao', 941416446, 1993-06-04),
+    ('Maria Fernandes', 'maria@jaguar.ao', 941416445, 1997-10-10),
+    ('Bruno Fernandes', 'bruno@jaguar.ao', 941416444, 2018-04-24);
