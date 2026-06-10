483041380a295d540caf8241328c7be257d25f0c
Julio Fernandes <jaguarfalcao93@gmail.com>
2026-06-10 19:30:59 +0100

    fase 2.5: CREATE TABLE turmas E RELACIONAMENTO com as tabelas cursos e alunos

---
+++ src/scripts_fase2.6.sql
@@ -0,0 +1,15 @@
+-- ============================================
+-- FASE 2: Banco de Dados - Jaguar Inteligente
+-- Aluno: Júlio Fernandes
+-- Data: 04/06/2026
+-- ============================================
+-- DELETE nas tabelas
+
+-- =========== deletando dados dos cursos ===============================
+
+-- altera o preço do curso Java Básico
+DELETE  FROM cursos WHERE id=2
+
+--  ============ deletando dados dos alunos =============================
+-- altera o nº de telefone da Maria Fernandes
+DELETE  FROM alunos WHERE id=5
